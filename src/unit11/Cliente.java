bb8c6a6a836a0326f3fe5375260d390b430010d2
antonreche <antonreche@gmail.com>
2015-05-29 21:45:10 +0200

    Implementada clase Cliente

--- src/unit11/Cliente.java
+++ src/unit11/Cliente.java
@@ -3,6 +3,8 @@
  */
 package unit11;
 
+import java.util.Scanner;
+
 /**
  * Clase Cliente con los datos de los datos personales y los tipos de carnet de los clientes
  * @author Antonio Reche Ramal
@@ -14,7 +16,7 @@ public class Cliente {
 	String dni;
 	boolean carnetTurismo;
 	boolean carnetFurgoneta;
-	boolean carnetCamión;
+	boolean carnetCamion;
 	boolean alquilado;		//true indica que tiene un vehículo ya alquilado
 	
 	/**
@@ -68,7 +70,7 @@ public class Cliente {
 	/**
 	 * @return the carnetFurgoneta
 	 */
-	public boolean isCarnetFurgoneta() {
+	public boolean getCarnetFurgoneta() {
 		return carnetFurgoneta;
 	}
 
@@ -82,21 +84,21 @@ public class Cliente {
 	/**
 	 * @return the carnetCamión
 	 */
-	public boolean isCarnetCamión() {
-		return carnetCamión;
+	public boolean getCarnetCamion() {
+		return carnetCamion;
 	}
 
 	/**
 	 * @param carnetCamión the carnetCamión to set
 	 */
-	public void setCarnetCamión(boolean carnetCamión) {
-		this.carnetCamión = carnetCamión;
+	public void setCarnetCamion(boolean carnetCamion) {
+		this.carnetCamion = carnetCamion;
 	}
 
 	/**
 	 * @return the alquilado
 	 */
-	public boolean isAlquilado() {
+	public boolean getAlquilado() {
 		return alquilado;
 	}
 
@@ -107,5 +109,65 @@ public class Cliente {
 		this.alquilado = alquilado;
 	}
 	
+	/**
+	 * Método para comprobar si la opción es válida ("s" o "n")
+	 * @param opcion	-->texto a comprobar
+	 * @return true si opcion es "s" o "n" false cualquier otro
+	 */
+	private boolean comprobarOpcion(String opcion)
+	{
+		if(opcion.equals("s"))
+			return true;
+		else if(opcion.equals("n"))
+			return true;
+		else{
+			System.out.println("¡Opción no válida!");
+			return false;
+		}
+	}
 	
+	/**
+	 * Método que va pidiendo por pantalla los datos para rellenar la ficha del cliente.
+	 */
+	public void rellenarDatos()
+	{
+		Scanner teclado=new Scanner(System.in);
+		String opcion;
+		System.out.println("Introduzca los datos del cliente: ");
+		System.out.print("\tNombre: ");
+		nombre=teclado.nextLine();
+		System.out.print("\nDNI: ");
+		dni=teclado.nextLine();
+		
+		do{
+			System.out.print("\n¿Tiene carnet para turismos? Si(s) No(n): ");
+			opcion=teclado.nextLine();
+		}while(!comprobarOpcion(opcion));
+		
+		if(opcion.equals("s"))
+			carnetTurismo=true;
+		else
+			carnetTurismo=false;
+		
+		do{
+			System.out.print("\n¿Tiene carnet para furgonetas? Si(s) No(n): ");
+			opcion=teclado.nextLine();
+		}while(!comprobarOpcion(opcion));
+		
+		if(opcion.equals("s"))
+			carnetFurgoneta=true;
+		else
+			carnetFurgoneta=false;
+		
+		do{
+			System.out.print("\n¿Tiene carnet para camión? Si(s) No(n): ");
+			opcion=teclado.nextLine();
+		}while(!comprobarOpcion(opcion));
+		
+		if(opcion.equals("s"))
+			carnetCamion=true;
+		else
+			carnetCamion=false;
+		
+	}
 }
